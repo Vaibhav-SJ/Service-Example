05a3add3ed5b928dd1cee94d14c64d187b1aadac
vj <prachi@gmail.com>
2018-07-09 11:43:22 +0530

    full code

--- app/src/main/java/com/example/appmomos/servicetest/MainActivity.java
+++ app/src/main/java/com/example/appmomos/servicetest/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.appmomos.servicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.appmomos.servicetest.Service.MyService;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    Button buttonStart, buttonStop,buttonNext;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonNext = findViewById(R.id.buttonNext);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonNext.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.buttonStart:
+                startService(new Intent(this, MyService.class));
+                break;
+
+            case R.id.buttonStop:
+                stopService(new Intent(this, MyService.class));
+                break;
+
+            case R.id.buttonNext:
+                Intent intent=new Intent(this,NextPageActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/appmomos/servicetest/Service/MyService.java
@@ -0,0 +1,51 @@
+package com.example.appmomos.servicetest.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.example.appmomos.servicetest.R;
+
+
+
+public class MyService extends Service
+{
+    MediaPlayer myPlayer;
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
+
+        myPlayer = MediaPlayer.create(this, R.raw.swag_se_swagat);
+        myPlayer.setLooping(false); // Set looping
+    }
+
+
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+        super.onStart(intent, startId);
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        myPlayer.start();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
+        myPlayer.stop();
+    }
+}
